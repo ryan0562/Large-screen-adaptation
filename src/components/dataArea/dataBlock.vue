97adecec23bb7b54e69a1351709b1949f339ed8b
王伟1 <wangwei1@mti-sh.cn>
2022-09-01 18:02:39 +0800

    动态配置模块

--- src/components/dataArea/dataBlock.vue
+++ src/components/dataArea/dataBlock.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="dataBlock" :style="styles">
     <div class="head" v-if="showHeader">
-      <div class="tit">{{ title }}</div>
-      <span class="title-right">
-        <slot name="headerright"></slot>
-      </span>
+      <span class="out-circle"></span>
+      <span class="tit">{{ title }}</span>
     </div>
     <component class="component" v-bind="component"></component>
   </div>
@@ -53,6 +51,42 @@ export default {
 .dataBlock {
   display: flex;
   flex-direction: column;
+  border: 1px solid transparent;
+  border-left-color: #0d3750;
+  border-right-color: #0d3750;
+  background-color: #11233c;
+  padding:10px;
+  .head {
+    height: 20px;
+    margin-bottom: 10px;
+    .out-circle {
+      width: 16px;
+      height: 16px;
+      border: 1px solid rgba(0, 255, 255, 0.35);
+      border-radius: 50%;
+      margin-right: 5px;
+      display: inline-block;
+      position: relative;
+      &:after {
+        content: '';
+        width: 6px;
+        height: 6px;
+        border: 2px solid #00ffff;
+        border-radius: 50%;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-left: -3px;
+        margin-top: -3px;
+        box-sizing: border-box;
+      }
+    }
+    .tit {
+      font-size: 16px;
+      line-height: 20px;
+      vertical-align: bottom;
+    }
+  }
   .component {
     flex: 1;
     min-height: 0;
