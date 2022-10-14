327db47d70e2cf10db0c861936a6c5e38d70a5fa
王伟1 <wangwei1@mti-sh.cn>
2022-10-14 11:16:17 +0800

    css 简易版优化

--- src/utils/sfc/parse.js
+++ src/utils/sfc/parse.js
@@ -1,16 +1,17 @@
 import { genStyleInjectionCode } from "./styleInjection";
-import { isEmpty, extend, generateId } from "./util";
-import { addStylesClient } from "./style-loader/addStylesClient";
+import { isEmpty, extend } from "./util";
+// import { addStylesClient } from "./style-loader/addStylesClient";
 
 
 
-export const genComponent = async function (sfcDescriptor, errors,viewId) {
+export const genComponent = async function (sfcDescriptor, errors, viewId) {
   let dynamicComponent = {};
   let demoComponent = {};
   const { template, script, styles, customBlocks } = sfcDescriptor;
   // errors
   if (errors && errors.length) {
     console.error(
+      `${viewId}\n` +
       `Error compiling template:\n\n` +
       errors.map((e) => `  - ${e}`).join("\n") +
       "\n\n"
@@ -18,12 +19,10 @@ export const genComponent = async function (sfcDescriptor, errors,viewId) {
   }
 
   const templateCode = template ? template.content.trim() : ``;
-  let scriptCode = script ? script.content.trim() : ``;
-  /* TODO */
-
-  const styleCodes = await genStyleInjectionCode(styles, viewId);
+  demoComponent.template = templateCode;
 
   // script
+  let scriptCode = script ? script.content.trim() : ``;
   if (!isEmpty(scriptCode)) {
     let componentScript = {};
     scriptCode = scriptCode.replace(
@@ -31,19 +30,25 @@ export const genComponent = async function (sfcDescriptor, errors,viewId) {
       "componentScript ="
     );
     eval(scriptCode);
-    // update component's content
     extend(demoComponent, componentScript);
   }
 
-  // template
-  // demoComponent.template = `<section id="${this.viewId}" class="result-box" >
-  //   ${templateCode}
-  // </section>`;
-  demoComponent.template = templateCode;
 
-  // style
-  // this.stylesUpdateHandler(styleCodes);
-  addStylesClient(viewId, styleCodes);
+  // 冗余处理
+  // addStylesClient(viewId, styleCodes);
+  // 简易版
+  const styleCodes = await genStyleInjectionCode(styles, viewId);
+  styleCodes.forEach(item => {
+    if (item.css) {
+      const style = document.createElement("style");
+      style.type = "text/css";
+      style.setAttribute('moduleName', viewId)
+      style.innerHTML = item.css;
+      document.head.appendChild(style);
+    }
+  })
+
+
 
   // update dynamicComponent
   extend(dynamicComponent, {
